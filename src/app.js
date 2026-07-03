ef2d354c831dd2144fe1a2f3dcd5cacc7c35c017
Edu-Mena <e.mena.baptista@gmail.com>
2026-07-03 10:17:28 +0100

    Configurar o Cors e CookieParser

--- src/app.js
+++ src/app.js
@@ -1,8 +1,15 @@
 import express from 'express';
+import helmet from 'helmet';
+import morgan from 'morgan';
+import cors from 'cors';
+import cookieParser from 'cookie-parser'
 
 const app = express();
 
 app.use(helmet());
+app.use(morgan('dev'));
+app.use(cors());
+app.use(cookieParser());
 
 app.get('/', (req, res) => {
     res.status(200).send('Rota principal criada!');
