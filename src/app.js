c9f45a4ba785a70f1fc4e0f9bfa53183eabce118
Edu-Mena <e.mena.baptista@gmail.com>
2026-07-06 15:58:07 +0100

    Try Find: Hosting bug 7

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import { corsOptions } from '#config/cors.js';
+import { errorHandler } from '#middlewares/errorHandler.middleware.js';
 
 console.log('[BOOT] início', Date.now());
 
@@ -8,6 +9,7 @@ const app = express();
 app.use(cors(corsOptions));
 
 app.get('/', (req, res) => res.send('ok'));
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, '0.0.0.0', () => {
