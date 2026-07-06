ec6ce7024c58011e9969239410ae55b71276ca3d
Edu-Mena <e.mena.baptista@gmail.com>
2026-07-06 15:28:49 +0100

    Try Find: Hosting bug 4

--- src/app.js
+++ src/app.js
@@ -1,18 +1,10 @@
 import express from 'express';
-import helmet from 'helmet';
-import morgan from 'morgan';
-import cors from 'cors';
-import cookieParser from 'cookie-parser';
+import { corsOptions } from '#config/cors.js';
 
 console.log('[BOOT] início', Date.now());
 
 const app = express();
-
-app.use(helmet());
-app.use(morgan('dev'));
-app.use(cors());
-app.use(cookieParser());
-app.use(express.json());
+app.use((await import('cors')).default(corsOptions));
 
 app.get('/', (req, res) => res.send('ok'));
 
