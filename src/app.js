0869f3445846a00a9525477baab15d167295c905
Edu-Mena <e.mena.baptista@gmail.com>
2026-07-06 15:19:00 +0100

    Try Find: Hosting bug 3

--- src/app.js
+++ src/app.js
@@ -1,5 +1,19 @@
 import express from 'express';
+import helmet from 'helmet';
+import morgan from 'morgan';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+
+console.log('[BOOT] início', Date.now());
+
 const app = express();
+
+app.use(helmet());
+app.use(morgan('dev'));
+app.use(cors());
+app.use(cookieParser());
+app.use(express.json());
+
 app.get('/', (req, res) => res.send('ok'));
 
 const PORT = process.env.PORT || 3000;
