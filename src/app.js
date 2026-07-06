f778f1c276d1041819712d001619f1f04dd6f183
Edu-Mena <e.mena.baptista@gmail.com>
2026-07-06 16:06:37 +0100

    Try Find: Hosting bug 8

--- src/app.js
+++ src/app.js
@@ -2,13 +2,16 @@ import express from 'express';
 import cors from 'cors';
 import { corsOptions } from '#config/cors.js';
 import { errorHandler } from '#middlewares/errorHandler.middleware.js';
+import authRouter from '#routes/auth.route.js';
 
 console.log('[BOOT] início', Date.now());
 
 const app = express();
 app.use(cors(corsOptions));
+app.use(express.json());
 
 app.get('/', (req, res) => res.send('ok'));
+app.use('/auth', authRouter);
 app.use(errorHandler);
 
 const PORT = process.env.PORT || 3000;
